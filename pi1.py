1b009cc9150b678e3f032a7e1da69e27bb404e4a
Tulio <tulio.tutui@gmail.com>
2023-02-24 18:10:23 +0100

    Add pi calculation

---
+++ pi1.py
@@ -0,0 +1,29 @@
+import random
+import multiprocessing
+import time
+
+def approx_pi(num_samples):
+    count = 0
+    for i in range(num_samples):
+        x = random.random()
+        y = random.random()
+        if x**2 + y**2 <= 1:
+            count += 1
+
+    return 4 * count / num_samples
+
+if __name__ == '__main__':
+    num_samples = 1000000
+    num_processes = 4
+    pool = multiprocessing.Pool(num_processes)
+    
+    time_start = time.time()
+    results = pool.map(approx_pi, [num_samples // num_processes] * num_processes)
+    pi_approx = sum(results) / num_processes
+    print(f"Time 4 processes: {time.time() - time_start}")
+
+    time_start = time.time()
+    results = approx_pi(num_samples)
+    print(f"Time 1 process: {time.time() - time_start}")
+
+    print('Approximation de Pi: {}'.format(pi_approx))
---
+++ pi2.py
@@ -0,0 +1,52 @@
+# %%
+import math
+from multiprocessing import Process, Queue
+import time
+from decimal import Decimal
+
+def gauss_legendre(num_iterations, output):
+    # Initialize variables
+    a = Decimal(1.0)
+    b = Decimal(1.0) / Decimal(2).sqrt()
+    t = Decimal(1.0) / Decimal(4.0)
+    p = Decimal(1.0)
+
+    # Iterate
+    for i in range(num_iterations):
+        a_new = (a + b) / Decimal(2.0)
+        b = (a * b).sqrt()
+        t -= p * (a - a_new) ** Decimal(2)
+        p *= Decimal(2.0)
+        a = a_new
+
+    # Calculate pi estimate
+    pi_estimate = ((a + b) ** Decimal(2)) / (Decimal(4.0) * t)
+    output.put(pi_estimate)
+
+# %%
+if __name__ == '__main__':
+    num_iterations = 1000000
+    num_processes = 4
+
+    output = Queue()
+
+    processes = [Process(target=gauss_legendre, args=(num_iterations // num_processes, output)) for _ in range(num_processes)]
+
+    time_start = time.time()
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    print(output)
+
+    pi_estimate = sum(output.get() for _ in range(num_processes)) / num_processes
+    print(f"Time 4 processes: {time.time() - time_start}")
+
+    time_start = time.time()
+    gauss_legendre(num_iterations, output)
+    print(f"Time 1 process: {time.time() - time_start}")
+
+    print(f"Estimated value of pi: {pi_estimate}")
+# %%
